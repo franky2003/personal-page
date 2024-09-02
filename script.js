8a9e4f7e77b18ca2f1dd1982da1086f9b0ad7558
franky2003 <nkgd2003@gmail.com>
2024-09-02 19:13:52 +0530

    updated code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.getElementById('welcome-button').addEventListener('click', function() {
-    alert('Myself Nandakrishnan, a rising developer with goals as big as the code I write, seeking to make every line count. With each project, I delve further into the world of development, striving to leave an impression one keystroke at a time.');
+    alert('Siuuuu!');
   });
   
   document.getElementById('profile-picture').addEventListener('mouseover', function() {
