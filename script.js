e0dda8a10cd8072b100391c0e9d7243b41f5bb36
franky2003 <nkgd2003@gmail.com>
2024-09-02 13:58:38 +0530

    Checked

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById('welcome-button').addEventListener('click', function() {
+    alert('Myself Nandakrishnan, a budding dev hopefully ');
+  });
+  
+  document.getElementById('profile-picture').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = '#EF476F'; 
+  });
+  
+  document.getElementById('profile-picture').addEventListener('mouseout', function() {
+    document.body.style.backgroundColor = ''; 
+  });
+  
